bcc3c344ece2a7070039e38ec51dc87e964a630d
hilmannanshorii <hilmanaanshori973@gmail.com>
2021-11-28 01:23:22 +0700

    Pertemuan 1 s/d 6

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Hilman</li>
+        <li>Nama Belakang : Anshori</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Tanjung Priok No.57 gg.17 Warakas 4</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Berenang</li>
+    </ul>
+</section>
